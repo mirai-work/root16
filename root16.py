edd7988c8ddabdff3476685dbccce2d84f48a1db
mirai-work <mfkensub@gmail.com>
2026-02-23 15:56:59 +0900

    Update root16.py

--- root16.py
+++ root16.py
@@ -84,8 +84,8 @@ class App:
             if t["life"] <= 0: self.trails.remove(t)
         mv = (2.4 if turbo else 1.6) if self.stage == 2 else (2.1 if turbo else 1.3)
         dx, dy = 0, 0
-        if pyxel.btn(pyxel.KEY_UP) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_UP): dy = -mv
-        elif pyxel.btn(pyxel.KEY_DOWN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DOWN): dy = mv
+        if pyxel.btn(pyxel.KEY_UP) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_UP): dy = -mv
+        elif pyxel.btn(pyxel.KEY_DOWN) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN): dy = mv
         if pyxel.btn(pyxel.KEY_LEFT): dx = -mv
         elif pyxel.btn(pyxel.KEY_RIGHT): dx = mv
         if not self.get_wall(self.px + dx, self.py): self.px += dx
@@ -219,5 +219,6 @@ class App:
         elif t == "P": pyxel.circb(x, y, 3, 12)
 
 App()
+
 
 
